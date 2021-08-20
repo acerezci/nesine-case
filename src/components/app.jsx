cf1e926d299874bdaf8ec73e92790c1be80917ea
Alperen Cerezci <alperencerezci@gmail.com>
2021-08-20 18:09:18 +0300

    initial commit

---
+++ src/components/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styles/app.scss';
+
+const App = () => (
+  <div className="app-container">
+    <div>Appjs</div>
+    <h1>TEST</h1>
+  </div>
+);
+
+export default App;
---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
+import '../public/favicon.ico';
+import './styles/reset.css';
+import './styles/index.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
